081b0ba724cba0b4345b2957febabc987748291e
Fabricio_Lucero <lucerofabricioagustin@gmail.com>
2024-11-11 20:51:26 -0300

    Comitnuevo

---
+++ Condicionales.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+a = random.randrange(0,100)
+if a % 2 == 0:
+    print("Multiplo de 2")
+elif a%3 ==0:
+    print("Multiplo de 3")
+else:
+    print("No es multiplo de 2 ni de 3")
+print(a)
+print("HOLA MUNDO")
+
